ce77b048a19b5c7a9053ed6e427ce0b07f0dfd5f
amy <amy87326@163.com>
2017-05-01 09:39:57 +0800

    seed admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+create_account = User.create([email: 'amy87326@163.com', password:'123456'， password_confirmation: '123456', is_admin: 'true'])
+puts "Admin account created."
