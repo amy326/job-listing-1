72f58c548931d7696eccb514946de05de04cfec0
amy <amy87326@163.com>
2017-05-01 12:54:18 +0800

    remove migration

--- db/migrate/20170427224817_newcolumn_for_time.rb
+++
@@ -1,6 +0,0 @@
-class NewcolumnForTime < ActiveRecord::Migration[5.0]
-  def change
-     add_column :jobs, :time_upper_bound, :datetime
-     add_column :jobs, :time_lower_bound, :datetime
-  end
-end
