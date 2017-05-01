3ef7aa6f5b763f0c0804a4fc1decb3ca111bf6da
amy <amy87326@163.com>
2017-05-01 13:18:57 +0800

    redo migrate succeed

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170427224652) do
+ActiveRecord::Schema.define(version: 20170501022937) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
-    t.datetime "time_lower_bound"
-    t.datetime "time_upper_bound"
     t.string   "contact_email"
     t.boolean  "is_hidden",        default: true
+    t.string   "image"
+    t.datetime "time_lower_bound"
+    t.datetime "time_upper_bound"
   end
 
   create_table "resumes", force: :cascade do |t|
